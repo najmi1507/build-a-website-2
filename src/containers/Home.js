58d03d3ddf8c94fa5415cbd9f9ca2ba798fd4391
Najmi Izzati <najmi.gnms@gmail.com>
2020-12-30 14:06:49 +0300

    new cities, change bg color

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,16 +13,17 @@ function Home() {
   // TODO
   return (
     <div className="flex flex-col h-screen bg-green-200">
-      
-      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-yellow-500"}/>
+      <City cityName={"Jakarta"} temp={"29°C"} color={"bg-yellow-500"} />
 
-      <City cityName={"Bandung"} temp={"24°C"} color={"bg-blue-500"} />
-      
-      <City cityName={"Jakarta"} temp={"29°C"} color={"bg-red-400"} />
+      <City cityName={"New York"} temp={"-1°C"} color={"bg-blue-500"} />
 
+      <City cityName={"Tokyo"} temp={"9°C"} color={"bg-red-400"} />
+
+      <City cityName={"Dubai"} temp={"23°C"} color={"bg-indigo-300"} />
+
+      <City cityName={"Kuala Lumpur"} temp={"27°C"} color={"bg-pink-300"} />
     </div>
   );
 }
-
 
 export default Home;
